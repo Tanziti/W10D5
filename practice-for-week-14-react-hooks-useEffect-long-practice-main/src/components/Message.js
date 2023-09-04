b09297908b182fde8da1dfad429e7ec28e2d6633
tanziti <tanzitian3@gmail.com>
2023-09-04 12:26:22 -0400

    useEffect_phase1

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -1,6 +1,8 @@
+import { useEffect } from 'react';
 function Message({ size }) {
-  console.log('Message', size);
-
+  useEffect(() => {
+    console.log('Message', size);
+  }, [size]);
   return (
     <div className="message medium">
       (Oh my! Your bird is naked!)
