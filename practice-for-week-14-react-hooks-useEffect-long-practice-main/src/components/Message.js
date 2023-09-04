ae62c205b6b828cbc4db7060a18b0450c7d9251c
tanziti <tanzitian3@gmail.com>
2023-09-04 13:38:48 -0400

    phase 4 done

--- practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
+++ practice-for-week-14-react-hooks-useEffect-long-practice-main/src/components/Message.js
@@ -1,12 +1,23 @@
 import { useEffect, useState } from 'react';
-function Message({ size }) {
+function Message({ size, featherCount }) {
   const [sizeClass, setSizeClass] = useState('');
   useEffect(() => {
     setSizeClass(size);
   }, [size]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (featherCount <= 0)
+      setMessage('Oh my! Your bird is naked!');
+    else if (featherCount >= 10) {
+      setMessage('Full turkey, lets eat!');
+    } else {
+      setMessage('Coming along...');
+    }
+  }, [featherCount])
   return (
-    <div className={`image-area ${sizeClass}`}>
-      (Oh my! Your bird is dry and not tasty!)
+    <div className={`message ${sizeClass}`}>
+      {message}
     </div>
   );
 };
